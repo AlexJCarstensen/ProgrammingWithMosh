b977e715a34bfa34b9678036c501d520686cdc3a
Alex Carstensen <matthacker.alex@gmail.com>
2015-11-27 19:47:15 +0100

    added two videos with AddVideo

--- VidzyDbFirst/VidzyDbFirst/Program.cs
+++ VidzyDbFirst/VidzyDbFirst/Program.cs
@@ -9,6 +9,10 @@ namespace VidzyDbFirst
     {
         static void Main(string[] args)
         {
+            var dbContext = new VidzyDbContext();
+            dbContext.AddVideo("Mission Impossible", new DateTime(1985, 7, 15), "Action");
+            dbContext.AddVideo("Titanic", DateTime.Now, "Romance");
+
         }
     }
 }
