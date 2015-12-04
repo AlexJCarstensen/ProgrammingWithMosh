fecb8b181bde2ebf8a76209cbef5a87ee4a778cb
Alex Carstensen <matthacker.alex@gmail.com>
2015-12-05 00:20:41 +0100

    break :)

---
+++ CSharpBasicsForBeginners/ArraysAndLists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Program1
+
+            var names = new List<string>();
+            while (true)
+            {
+                Console.Write("Please enter a friend name: ");
+                var name = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(name))
+                   break;
+
+                names.Add(name);
+            }
+
+            #endregion
+        }
+    }
+}
