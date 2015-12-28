419097e121dfde7f48adb1a115ec00fc45a6fd63
Alex Carstensen <matthacker.alex@gmail.com>
2015-12-28 22:02:32 +0100

    workingwithfiles done

--- CSharpBasicsForBeginners/WorkingWithFiles/Program.cs
+++ CSharpBasicsForBeginners/WorkingWithFiles/Program.cs
@@ -11,9 +11,14 @@ namespace WorkingWithFiles
     {
         static void Main(string[] args)
         {
-            var countWords = WordsInTextFile(Directory.GetCurrentDirectory() + @"\text.txt");
+            // var countWords = WordsInTextFile(Directory.GetCurrentDirectory() + @"\text.txt");
+            // Console.WriteLine(countWords);
 
-            Console.WriteLine(countWords);
+            var repeatedWords = MostRepeatedWords(Directory.GetCurrentDirectory() + @"\text.txt");
+            foreach (var repeatedWord in repeatedWords)
+            {
+                Console.WriteLine(repeatedWord);
+            }
         }
 
         public static int WordsInTextFile(string path)
@@ -27,9 +32,51 @@ namespace WorkingWithFiles
             return count;
         }
 
-        public static string MostRepeatedWords(string path)
+        public static List<string> MostRepeatedWords(string path)
         {
-            return "";
+            var words = File.ReadAllText(path);
+            var wordArray = words.Split(' ');
+            var repeatedWords = new List<string>();
+            var uniqueWords = new List<string>();
+
+            foreach (var word in wordArray)
+            {
+                if (!uniqueWords.Contains(word))
+                    uniqueWords.Add(word);
+                else
+                {
+                    if(!repeatedWords.Contains(word))
+                        repeatedWords.Add(word);
+                }
+            }
+
+            var numString = new List<string>();
+            foreach (var repeatedWord in repeatedWords)
+            {
+                var count = 0;
+                for (int i = 0; i < wordArray.Length; i++)
+                {
+                    if (wordArray[i].Contains(repeatedWord))
+                        count++;
+                }
+
+                var strBuild = new StringBuilder();
+                var str = "";
+                str = strBuild.Append(Convert.ToString(count) + ' ' + repeatedWord).ToString();
+                numString.Add(str);
+                numString.Sort();
+            }
+            foreach (var item in numString)
+            {
+                Console.WriteLine(item);
+            }
+            var mostRepeated = new List<string>
+            {
+                numString.ElementAt(numString.Count - 1).Remove(0, 2),
+                numString.ElementAt(numString.Count - 2).Remove(0, 2),
+                numString.ElementAt(numString.Count - 3).Remove(0, 2)
+            };
+            return mostRepeated;
         }
     }
 }
