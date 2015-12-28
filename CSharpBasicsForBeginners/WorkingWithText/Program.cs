18e88e18bfaa8721a9ea902d73b32df82da64e7d
Alex Carstensen <matthacker.alex@gmail.com>
2015-12-28 21:13:42 +0100

    Procedural programming WorkingWithText

--- CSharpBasicsForBeginners/WorkingWithText/Program.cs
+++ CSharpBasicsForBeginners/WorkingWithText/Program.cs
@@ -12,28 +12,13 @@ namespace WorkingWithText
         {
             #region Program1
 
-            //Console.WriteLine("Please input a few numbers seperated by a hyphen.");
-            //var input = Console.ReadLine();
+            Console.WriteLine("Please input a few numbers seperated by a hyphen.");
+            var input = Console.ReadLine();
 
-            //var numbers = new List<int>();
+            var consecutive = IsConsecutiveList(input);
 
-            //foreach (var number in input.Split('-'))
-            //{
-            //    numbers.Add(Convert.ToInt32(number));
-            //}
-
-            //var consecutive = true;
-            //for (int i = 1; i < numbers.Count; i++)
-            //{
-            //    if (numbers[i] != numbers[i - 1] + 1 && numbers[i] != numbers[i - 1] - 1)
-            //    {
-            //        consecutive = false;
-            //        break;
-            //    }
-            //}
-
-            //var message = consecutive ? "Consecutive" : "Not Consecutive";
-            //Console.WriteLine(message);
+            var message = consecutive ? "Consecutive" : "Not Consecutive";
+            Console.WriteLine(message);
 
 
             #endregion
@@ -42,29 +27,9 @@ namespace WorkingWithText
 
             //Console.WriteLine("Please input a few numbers seperated by a hyphen.");
             //var input = Console.ReadLine();
-            //if (string.IsNullOrWhiteSpace(input)) return;
 
-            //var numbers = new List<int>();
-
-            //foreach (var number in input.Split('-'))
-            //{
-            //    numbers.Add(Convert.ToInt32(number));
-            //}
-
-            //var uniqueNumbers = new List<int>();
-            //var dublicates = false;
-
-            //foreach (var number in numbers)
-            //{
-            //    if(!uniqueNumbers.Contains(number))
-            //        uniqueNumbers.Add(number);
-            //    else
-            //    {
-            //        dublicates = true;
-            //        break;
-            //    }
-            //}
-            //if(dublicates)
+            //var dublicates = ContainsDublicate(input);
+            //if (dublicates)
             //    Console.WriteLine("Duplicate");
 
             #endregion
@@ -74,35 +39,9 @@ namespace WorkingWithText
             //Console.WriteLine("please input a 24-hour format time");
             //var input = Console.ReadLine();
 
-            //if (string.IsNullOrWhiteSpace(input))
-            //{
-            //    Console.WriteLine("Invalid Time");
-            //    return;
-            //}
+            //var result = CorrectTimeFormat(input);
 
-            //var time = input.Split(':');
-            //if (time.Length != 2)
-            //{
-            //    Console.WriteLine("Invalid Time");
-            //    return;
-            //}
-
-            //try
-            //{
-            //    var hour = Convert.ToInt32(time[0]);
-            //    var minute = Convert.ToInt32(time[1]);
-
-            //    if(hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
-            //        Console.WriteLine("OK");
-            //    else
-            //        Console.WriteLine("Invalid Time");
-
-            //}
-            //catch (Exception)
-            //{
-            //    Console.WriteLine("Invalid Time");
-
-            //}
+            //Console.WriteLine(result ? "Ok" : "Invalid Time");
 
             #endregion
 
@@ -111,38 +50,122 @@ namespace WorkingWithText
             //Console.WriteLine("enter words");
             //var input = Console.ReadLine();
 
-            //if (string.IsNullOrWhiteSpace(input))
-            //{
-            //    Console.WriteLine("No input");
-            //    return;
-            //}
 
-            //var variableName = "";
-            //foreach (var word in input.Split(' '))
-            //{
-            //    var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
-            //    variableName += wordWithPascalCase;
-            //}
+            //var pascalCase = WordsToPascalCase(input);
 
-            //Console.WriteLine(variableName);
+            //Console.WriteLine(pascalCase);
+
             #endregion
 
             #region Program5
 
-            Console.WriteLine("Enter a word: ");
-            var input = Console.ReadLine();
+            //Console.WriteLine("Enter a word: ");
+            //var input = Console.ReadLine();
 
-            var vowels = new List<char>(new[] {'a','e','o','u','i'});
+            //var vowelsCount = CountVowels(input);
+
+            //Console.WriteLine(vowelsCount);
+
+            #endregion
+        }
+
+        public static bool IsConsecutiveList(string input)
+        {
+            var numbers = new List<int>();
+
+            foreach (var number in input.Split('-'))
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+
+            var consecutive = true;
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] != numbers[i - 1] + 1 && numbers[i] != numbers[i - 1] - 1)
+                {
+                    consecutive = false;
+                }
+            }
+            return consecutive;
+        }
+        public static bool ContainsDublicate(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return false;
+
+            var numbers = new List<int>();
+
+            foreach (var number in input.Split('-'))
+            {
+                numbers.Add(Convert.ToInt32(number));
+            }
+
+            var uniqueNumbers = new List<int>();
+            var dublicates = false;
+
+            foreach (var number in numbers)
+            {
+                if (!uniqueNumbers.Contains(number))
+                    uniqueNumbers.Add(number);
+                else
+                {
+                    dublicates = true;
+                    
+                }
+            }
+            return dublicates;
+        }
+        public static bool CorrectTimeFormat(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            var time = input.Split(':');
+            if (time.Length != 2)
+            {
+                return false;
+            }
+
+            try
+            {
+                var hour = Convert.ToInt32(time[0]);
+                var minute = Convert.ToInt32(time[1]);
+
+                return (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59);
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public static string WordsToPascalCase(string input )
+        {
+
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return "";
+            }
+            var variableName = "";
+            foreach (var word in input.Split(' '))
+            {
+                var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+                variableName += wordWithPascalCase;
+            }
+            return variableName;
+        }
+        public static int CountVowels(string input)
+        {
+            var vowels = new List<char>(new[] { 'a', 'e', 'o', 'u', 'i' });
             var vowelsCount = 0;
 
-            foreach (var character in input)
+            foreach (var character in input.ToLower())
             {
                 if (vowels.Contains(character))
                     vowelsCount++;
             }
-            Console.WriteLine(vowelsCount);
-
-            #endregion
+            return vowelsCount;
         }
     }
 }
