c320af8fb783707a2680c0fe5b84b8e7f4b1c1c5
Alex Carstensen <matthacker.alex@gmail.com>
2016-01-30 20:39:54 +0100

    Stack exercise completed

---
+++ CSharpIntermediateInheritance/Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ CSharpIntermediateInheritance/Stack/Stack.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+
+namespace Stack
+{
+    public class Stack  
+    {
+       
+        private readonly ArrayList _list = new ArrayList();
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+                _list.Insert(0, obj);
+            else
+                throw new InvalidOperationException("The object is null");
+
+           
+        }
+
+        public object Pop()
+        {
+            if (_list == null) throw new InvalidOperationException("The list is empty");
+            var removed = _list[0];
+            _list.Remove(_list[0]);
+            return removed;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+    }
+}
